dc65a77d7c1adb65930605552b47b63a52d56db2
MatuAcosta <acosta_matias8@hotmail.com>
2021-10-14 16:33:45 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from lista import lista 
+import random
 
 procesos = lista ()
 
 for i in range (1,10):
     proceso = {
         'id': i, 
-        'tamano': 150,
+        'tamano': random.randint(1,250),
         'ta': i,
         'ti': i+1
     }
     procesos.insert(proceso)
-
 
 procesos.imprimir()
