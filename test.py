0b0a41b686efa80178cb4677e4a8ef9fc11806e2
MatuAcosta <acosta_matias8@hotmail.com>
2021-10-14 16:47:32 -0300

    Update test.py

--- test.py
+++ test.py
@@ -2,19 +2,27 @@ from lista import lista
 import random
 
 particiones = lista ()
+acum = 1
 
-
-for i in range (1,10):
-    particion = {
-        'id': i, 
-        'tamano': random.randint(1,250),
-        'fragmentacion':0,
-        'idproc': None,
-        'ta': i,
-        'ti': i+1,
-        'dirInicio': 0,
-        'estado': ''
-    }
+for i in range (1,5):
+    if i == 1: 
+        particion = { 
+            'id': i,
+            'tamano': 100,
+            'dirInicio': 0
+        }
+    else:   
+        particion = {
+            'id': i, 
+            'tamano': random.randint(1,250),
+            'fragmentacion':0,
+            'idproc': None,
+            'ta': i,
+            'ti': i+1,
+            'dirInicio': acum,
+            'estado': ''
+        }
+    acum += particion['tamano']
     particiones.insert(particion)
 
 particiones.imprimir()
