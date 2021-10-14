09466d22ae1be949b309ba5bf4254669b70376c0
MatuAcosta <acosta_matias8@hotmail.com>
2021-10-14 12:42:09 -0300

    primer commit
    clase nodo, lista y una prueba para imprimir la lista

---
+++ lista.py
@@ -0,0 +1,22 @@
+from nodo import nodo
+
+class lista: 
+    def __init__(self):
+        self.head = None
+    
+    # Método para agregar elementos al final de la linked list (cola)
+    def insert(self, data):
+        if not self.head:
+            self.head = nodo(data)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = nodo(data)
+    
+    def imprimir(self):
+        node = self.head
+        while node != None:
+            print( node.data, end= '-->')            
+            node = node.next
+        print('Null')
---
+++ test.py
@@ -0,0 +1,12 @@
+from lista import lista 
+from nodo import nodo 
+
+list = lista ()
+
+list.insert(nodo(1))
+list.insert(nodo(2))
+list.insert(nodo(3))
+list.insert(nodo(4))
+list.insert(nodo(5))
+
+list.imprimir()
