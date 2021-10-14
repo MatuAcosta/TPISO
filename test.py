529b891321c414472a02901d2f5c341c35ef0a54
MatuAcosta <acosta_matias8@hotmail.com>
2021-10-14 17:09:23 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,39 @@
 from lista import lista 
 import random
 
-particiones = lista ()
-acum = 0
 
-for i in range (1,5):
-    if i == 1: 
-        particion = { 
+def crearproceso ():
+    cantprocesos = 5 #podemos preguntarle al usuario
+    procesos = lista ()  
+    for i in range (cantprocesos):
+        proceso = { 
             'id': i,
-            'tamano': 100,
-            'dirInicio': 0
+            'tamano': random.randint(1,250), #podemos preguntar al usuario
+            'ta':  random.randint(1,250), #podemos preguntar
+            'ti':  random.randint(1,250) #podemos preguntar
         }
-    else:   
-        particion = {
-            'id': i, 
-            'tamano': random.randint(1,250),
-            'fragmentacion':0,
-            'idproc': None,
-            'dirInicio': acum,
-            'estado': ''
-        }
-    acum += (particion['tamano']+1)
-    particiones.insert(particion)
+        procesos.insert(proceso)
 
-particiones.imprimir()
+
+def crearparticiones ():
+    particiones = lista ()
+    acum = 0
+    for i in range (1,5):
+        if i == 1: 
+            particion = { 
+                'id': i,
+                'tamano': 100,
+                'dirInicio': 0
+            }
+        else:   
+            particion = {
+                'id': i, 
+                'tamano': ,
+                'fragmentacion':0,
+                'idproc': None,
+                'dirInicio': acum,
+                'estado': ''
+            }
+        acum += (particion['tamano']+1)
+        particiones.insert(particion)
+    particiones.imprimir()
