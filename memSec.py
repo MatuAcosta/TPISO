b859021d8647a181498b5d222c0089328de667ea
MatuAcosta <acosta_matias8@hotmail.com>
2021-11-01 20:06:42 -0300

    MemSec

---
+++ memSec.py
@@ -0,0 +1,38 @@
+from proceso import Proceso 
+class memoriaSec: 
+    def __init__(self):
+        self.procSusp = []
+
+    def getProcesos (self):
+        return self.procSusp
+
+    def agregarProceso(self,proceso):
+        self.procSusp.append(proceso)
+        return self.procSusp
+
+    def quitarProceso (self,proceso): 
+        self.procSusp.remove(proceso)
+
+    def mostrarDisco (self):
+        for proceso in self.procSusp: 
+            print (proceso.id)
+    
+
+#---------PRUEBA-------
+#disco = memoriaSec ()
+#for x in range (5): 
+#    proceso = Proceso (x,100,2,x+1)
+#    disco.agregarProceso(proceso)
+
+#disco.mostrarDisco()
+#ti = 10000
+#proc = disco.getProcesos()
+
+#eliminar = None
+#for proceso in proc: 
+#    if (ti>proceso.ti): 
+#        ti = proceso.ti
+#        eliminar = proceso
+
+#disco.quitarProceso(eliminar)
+#disco.mostrarDisco()
