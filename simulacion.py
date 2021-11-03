341612ec398ed63776ac51e70e716596562b78ef
MatuAcosta <acosta_matias8@hotmail.com>
2021-11-03 12:44:52 -0300

    Comienzo Simulacion
    Falta mucho

--- simulacion.py
+++ simulacion.py
@@ -1,26 +1,57 @@
 from so import SistemaOperativo
+from memSec import memoriaSec
+from cpu import Cpu
+from particion import Particion 
+from proceso import Proceso
+from memoria import Memoria
 
 reloj = 0 
 so = SistemaOperativo ()
 nuevos = so.cola_nuevos 
-listos = so.cola_listos
+listos = so.memoria.cola_listos
 disco = so.disco
-## Comenzamos creando los procesos 
 
+def mostrarNuevos():
+    for proceso in nuevos:
+        print(proceso.getData(),'instante:',reloj)
+
+def mostrarListos():
+    for proceso in listos :
+        print(proceso.getData(),'instante:',reloj)
+
+def mostrarProcesos():
+    for proceso in so.procesos:
+        print(proceso.getData(),'instante:',reloj)
+
+
+## Comenzamos creando los procesos 
 so.crearprocesos()
 
 ## la simulacion ira hasta q las 3 colas esten vacias.
 #nuevos.len != 0 or listos.len !=0 or disco.len !=0 esta deberia ser la condicion de fin 
+
 while (reloj < 10):
     so.cargarNuevos()
     so.bestFit()
+    
+    mostrarListos() 
+    print ('reloj',reloj)
+    reloj +=1
 
- # if (self.disco.len < 6):
- #           self.disco.agregarProceso(proceso)
- #       particiones = self.memoria.particiones
- #       for i in range (len(particiones)): 
- #           if particiones[i].tamano >= proceso.tamano :
- #               for proc in self.cola_listos : 
- #                   if proceso.ti < proc.ti :
- #                       for j in range (len(particiones)-i)
+
+
+#print('-' * 50)
+#so.mostrarNuevos()
+#so.bestFit()
+#print('-' * 50)
+# so.mostrarNuevos()
+#so.disco.mostrarDisco()
+#print('-' * 50)
+#print('Listos')
+#so.mostrarListos()
+#print('-' * 50)
+#memoria.mostrarParticiones()
+#print('-' * 50)
+#so.srtf()
+#so.mostrarCpu()
     
