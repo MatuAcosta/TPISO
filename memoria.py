699e83e3e97e7ee2d29699c8d9908e513db44085
gonza1020 <gonzalozoloaga44@gmail.com>
2021-11-10 11:47:04 -0300

    Agregando metodo para visualizar el estado del simulador en cada instante de tiempo

--- memoria.py
+++ memoria.py
@@ -10,7 +10,7 @@ class Memoria():
         particiones = []
         tamanos = [100,250,120,60]
         acum = 101
-        particion = Particion(0, tamanos[0], 0, 0, 'Ocupado')
+        particion = Particion(0, tamanos[0], 0, 0, 'RESERVADO PARA SO')
         particiones.append(particion)
         for i in range (1,4):
             particion = Particion(i, tamanos[i], acum, 0)
